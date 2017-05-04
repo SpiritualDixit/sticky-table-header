f0fce7661beebbfbde9fc8d2758193678343d15b
Deepak Dixit <deepakdixit0001@gmail.com>
2017-05-05 01:52:47 +0530

    Updated Odoo manifest file

--- sticky_table_header/__openerp__.py
+++ sticky_table_header/__openerp__.py
@@ -18,23 +18,17 @@ Very helpful for a huge list view .
     'author': "Deepak Dixit",
     'website': "deepakdixit0001@gmail.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Widget',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
+    # Dependencies 
     'depends': ['base','web'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    
+    'application': False,    
+    'installable': True
 }
